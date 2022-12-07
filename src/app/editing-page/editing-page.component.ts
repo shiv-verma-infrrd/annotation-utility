65bb967520ca5aae7037f1d07ab1ad8e455596dc
bhagyam-pandey-infrrd <bhagyampandey@infrrd.ai>
2022-12-07 21:15:55 +0530

    final changes

--- src/app/editing-page/editing-page.component.ts
+++ src/app/editing-page/editing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { AfterViewInit, Component,ViewChild } from '@angular/core';
 import cord1 from 'src/assets/page1.json';
 import { ApiDataService } from '../services/api-data.service';
 @Component({
@@ -6,13 +6,15 @@ import { ApiDataService } from '../services/api-data.service';
   templateUrl: './editing-page.component.html',
   styleUrls: ['./editing-page.component.css']
 })
-export class EditingPageComponent 
+export class EditingPageComponent implements AfterViewInit
 {
   @ViewChild('header_input_ref') header_input_ref:any;
   @ViewChild('question_input_ref') question_input_ref:any;
   @ViewChild('answer_input_ref') answer_input_ref:any;
+  @ViewChild('i_r') img_ref: any;
+  @ViewChild('sv') svg_ref: any;
+  @ViewChild('entity_rect_ref') entity_rect_ref:any;
 
-  // apiDocuments:any;
   constructor(private apiData: ApiDataService) { 
     
     apiData.get_ocr_data("638fad525d2501fd2fa5ae41").subscribe((data)=>{
@@ -22,20 +24,39 @@ export class EditingPageComponent
     })
   }
 
-
   title:string="";
   selected_input_name:string="";
   entity_type_button:string="Header";
-  coordinate_array:any=cord1.ocrCoordinates;
+  coordinate_array:any=cord1.form;
 
-  // question_input_index:number[]=[];
+  img_length: number=0;
+  img_width: number=0;
+
+  l: number = 0;
+  c: number = 0;
+
+  times:number=1;
+
+  temp_array_1:number[]=[];
+  temp_array_2:number[]=[];
+
+  question_input_index:number[][]=[];
   question_array:string[]=[];
-  // answer_input_index:number[]=[];
+  
+  answer_input_index:number[][]=[];
   answer_array:string[]=[];
-  // header_input_index:number[]=[];
+  
+  header_input_index:number[][]=[];
   header_array:string[]=[];
 
   ngAfterViewInit(): void{
+   
+    setTimeout(()=>{
+      this.img_length=this.img_ref.nativeElement.offsetHeight;
+      this.img_width=this.img_ref.nativeElement.offsetWidth;
+      this.l=this.img_ref.nativeElement.offsetHeight;
+      this.c=this.img_ref.nativeElement.offsetWidth;
+    },1000);
   }
 
   token_mouse_enter(data:any)
@@ -54,42 +75,42 @@ export class EditingPageComponent
     {
       if(this.header_input_ref.nativeElement.value!="")
       {
-        // this.header_input_index.push(data);
-        this.header_input_ref.nativeElement.value+= " " + this.coordinate_array[data].word;
+        this.temp_array_1.push(data);
+        this.header_input_ref.nativeElement.value+= " " + this.coordinate_array[data].text;
       }
       else
       {
-        // this.header_input_index=[];
-        // this.header_input_index.push(data);
-        this.header_input_ref.nativeElement.value+=this.coordinate_array[data].word;
+        this.temp_array_1=[];
+        this.temp_array_1.push(data);
+        this.header_input_ref.nativeElement.value+=this.coordinate_array[data].text;
       }
     }
     else if(this.selected_input_name=='question')
     {
       if(this.question_input_ref.nativeElement.value!="")
       {
-        // this.question_input_index.push(data);
-        this.question_input_ref.nativeElement.value+= " " + this.coordinate_array[data].word;
+        this.temp_array_1.push(data);
+        this.question_input_ref.nativeElement.value+= " " + this.coordinate_array[data].text;
       }
       else
       {
-        // this.question_input_index=[];
-        // this.question_input_index.push(data);
-        this.question_input_ref.nativeElement.value+=this.coordinate_array[data].word;
+        this.temp_array_1=[];
+        this.temp_array_1.push(data);
+        this.question_input_ref.nativeElement.value+=this.coordinate_array[data].text;
       }
     }
     else if(this.selected_input_name=='answer')
     {
       if(this.answer_input_ref.nativeElement.value!="")
       {
-        // this.answer_input_index.push(data);
-        this.answer_input_ref.nativeElement.value+= " " + this.coordinate_array[data].word;
+        this.temp_array_2.push(data);
+        this.answer_input_ref.nativeElement.value+= " " + this.coordinate_array[data].text;
       }
       else
       {
-        // this.answer_input_index=[];
-        // this.answer_input_index.push(data);
-        this.answer_input_ref.nativeElement.value+=this.coordinate_array[data].word;
+        this.temp_array_2=[];
+        this.temp_array_2.push(data);
+        this.answer_input_ref.nativeElement.value+=this.coordinate_array[data].text;
       }
     }
   }
@@ -116,6 +137,9 @@ export class EditingPageComponent
 
   select_input(input_type:any)
   {
+    this.entity_rect_ref.nativeElement.style.height=0;
+    this.entity_rect_ref.nativeElement.style.width=0;
+    
     this.selected_input_name=input_type; 
   }
 
@@ -131,6 +155,9 @@ export class EditingPageComponent
       {
         // console.log(this.header_input_index);
         this.header_array.push(this.header_input_ref.nativeElement.value);
+        this.header_input_index.push(this.temp_array_1);
+
+        this.temp_array_1=[];
         this.header_input_ref.nativeElement.value="";
       }
     }
@@ -145,22 +172,33 @@ export class EditingPageComponent
         // console.log(this.question_input_index);
         // console.log(this.answer_input_index);
         this.question_array.push(this.question_input_ref.nativeElement.value);
+        this.question_input_index.push(this.temp_array_1);
+        this.temp_array_1=[];
         this.question_input_ref.nativeElement.value="";
+
         this.answer_array.push(this.answer_input_ref.nativeElement.value);
+        this.answer_input_index.push(this.temp_array_2);
+        this.temp_array_2=[];
         this.answer_input_ref.nativeElement.value="";
       }
     }
   }
   delete_entity(ind:number,t:string)
   {
+    this.entity_rect_ref.nativeElement.style.height=0;
+    this.entity_rect_ref.nativeElement.style.width=0;
+
      if(t=='H')
      {
       this.header_array.splice(ind,1);
+      this.header_input_index.splice(ind,1);
      }
      else
      {
       this.question_array.splice(ind,1);
       this.answer_array.splice(ind,1);
+      this.question_input_index.splice(ind,1);
+      this.answer_input_index.splice(ind,1);
      }
   }
   clear_input(type:any)
@@ -171,6 +209,105 @@ export class EditingPageComponent
      this.question_input_ref.nativeElement.value="";
      else
      this.answer_input_ref.nativeElement.value="";
-  } 
+  }
+  
+  zoom_in() {
+    if (this.times >= 2.5) return;
 
+    this.times += 0.2;
+
+    this.img_ref.nativeElement.style.height = this.l * this.times + 'px';
+    this.img_ref.nativeElement.style.width = this.c * this.times + 'px';
+
+    this.img_length = this.l * this.times;
+    this.img_width = this.c * this.times;
+
+    for (let i = 0; i < this.coordinate_array.length; i++) {
+      this.coordinate_array[i].box[0] = (cord1.form[i].box[0]/(this.times-0.2)) * this.times;
+      this.coordinate_array[i].box[1] = (cord1.form[i].box[1]/(this.times-0.2)) * this.times;
+      this.coordinate_array[i].box[2] = (cord1.form[i].box[2]/(this.times-0.2)) * this.times;
+      this.coordinate_array[i].box[3]= (cord1.form[i].box[3]/(this.times-0.2)) * this.times;
+    }
+  }
+
+  zoom_out() {
+
+    this.times -= 0.2;
+
+    this.img_ref.nativeElement.style.height = this.l * this.times + 'px';
+    this.img_ref.nativeElement.style.width = this.c * this.times + 'px';
+
+    this.img_length = this.l * this.times;
+    this.img_width = this.c * this.times;
+
+   
+    for (let i = 0; i < this.coordinate_array.length; i++) {
+      this.coordinate_array[i].box[0] = (cord1.form[i].box[0]/(this.times+0.2)) * this.times;
+      this.coordinate_array[i].box[1] = (cord1.form[i].box[1]/(this.times+0.2)) * this.times;
+      this.coordinate_array[i].box[2] = (cord1.form[i].box[2]/(this.times+0.2)) * this.times;
+      this.coordinate_array[i].box[3]= (cord1.form[i].box[3]/(this.times+0.2)) * this.times;
+    }
+  }
+
+  entity_click(type:string,index:number)
+  {
+    var x=999,y=999,x2=-1,y2=-1;
+     if(type=='q')
+     { 
+     for(let i=0;i<this.question_input_index[index].length;i++)
+     {
+      if(x>this.coordinate_array[this.question_input_index[index][i]].box[0]) 
+       x=this.coordinate_array[this.question_input_index[index][i]].box[0]; 
+      
+      if(y>this.coordinate_array[this.question_input_index[index][i]].box[1]) 
+       y=this.coordinate_array[this.question_input_index[index][0]].box[1];
+
+      if(x2<this.coordinate_array[this.question_input_index[index][i]].box[2])  
+      x2=this.coordinate_array[this.question_input_index[index][i]].box[2]
+
+      if(y2<this.coordinate_array[this.question_input_index[index][i]].box[3])  
+      y2=this.coordinate_array[this.question_input_index[index][i]].box[3]
+     }
+     }
+     else if(type=='a')
+     {  
+      for(let i=0;i<this.answer_input_index[index].length;i++)
+      {
+       if(x>this.coordinate_array[this.answer_input_index[index][i]].box[0]) 
+        x=this.coordinate_array[this.answer_input_index[index][i]].box[0]; 
+       
+       if(y>this.coordinate_array[this.answer_input_index[index][i]].box[1]) 
+        y=this.coordinate_array[this.answer_input_index[index][0]].box[1];
+ 
+       if(x2<this.coordinate_array[this.answer_input_index[index][i]].box[2])  
+       x2=this.coordinate_array[this.answer_input_index[index][i]].box[2]
+ 
+       if(y2<this.coordinate_array[this.answer_input_index[index][i]].box[3])  
+       y2=this.coordinate_array[this.answer_input_index[index][i]].box[3]
+      }
+     }
+     else
+     { 
+      for(let i=0;i<this.header_input_index[index].length;i++)
+      {
+       if(x>this.coordinate_array[this.header_input_index[index][i]].box[0]) 
+        x=this.coordinate_array[this.header_input_index[index][i]].box[0]; 
+       
+       if(y>this.coordinate_array[this.header_input_index[index][i]].box[1]) 
+        y=this.coordinate_array[this.header_input_index[index][0]].box[1];
+ 
+       if(x2<this.coordinate_array[this.header_input_index[index][i]].box[2])  
+       x2=this.coordinate_array[this.header_input_index[index][i]].box[2]
+ 
+       if(y2<this.coordinate_array[this.header_input_index[index][i]].box[3])  
+       y2=this.coordinate_array[this.header_input_index[index][i]].box[3]
+      } 
+     }
+
+     this.entity_rect_ref.nativeElement.style.x=x;
+     this.entity_rect_ref.nativeElement.style.y=y;
+     
+     this.entity_rect_ref.nativeElement.style.height=y2-y;
+     this.entity_rect_ref.nativeElement.style.width=x2-x;
+  }
 }
