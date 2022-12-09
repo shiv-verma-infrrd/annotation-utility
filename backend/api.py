dc1c6e0ff3935f9a725247d8e072dd501c5f9164
aakash-vc-infrrd <aakashvc@infrrd.ai>
2022-12-09 10:13:16 +0530

    Minor changes to APIs and auth integration

--- backend/api.py
+++ backend/api.py
@@ -1,83 +1,21 @@
-# from flask import Flask, request, jsonify, Response, json
-# from flask_jwt_extended import JWTManager, jwt_required, create_access_token
-# from pymongo import MongoClient
-
-# client = MongoClient("mongodb://localhost:27017/")
-# database = client["CorrectionUI"]
-# user = database["Users"]
-
-# app = Flask(__name__)
-# jwt = JWTManager(app)
-
-# app.config["JWT_SECRET_KEY"] = "key"
-
-# @app.route("/api/register", methods=["POST"])
-# def register():
-#     email = request.form["email"]
-#     test = user.find_one({"email": email})
-#     if test:
-#         return jsonify(message="User Already Exist"), 409
-#     else:
-#         name = request.form["name"]
-#         password = request.form["password"]
-#         user_info = dict(name=name, email=email, password=password)
-#         user.insert_one(user_info)
-#         return jsonify(message="User added sucessfully"), 201
-
-# @app.route("/api/login", methods=["POST"])
-# def login():
-#     if request.is_json:
-#         email = request.json["email"]
-#         password = request.json["password"]
-#     else:
-#         email = request.form["email"]
-#         password = request.form["password"]
-
-#     test = user.find_one({"email": email,"password":password})
-#     if test:
-#         access_token = create_access_token(identity=email)
-#         return jsonify(message="Login Succeeded!", access_token=access_token), 201
-#     else:
-#         return jsonify(message="Bad Email or Password"), 401
-
-# class MongoAPI:
-#     def __init__(self, collection):
-#         self.client = MongoClient("mongodb://localhost:27017/")
-#         database = 'CorrectionUI'
-#         cursor = self.client[database]
-#         self.collection = cursor[collection]
-        
-#     def read(self):
-#         documents = self.collection.find()
-#         output = [{item: data[item] for item in data} for data in documents]
-#         return output
-
-# @app.route('/api/batches', methods=['GET'])
-# @jwt_required()
-# def batches_read():
-#     obj1 = MongoAPI('Batches')
-#     response = {"batchList": obj1.read()}
-#     return Response(response=json.dumps(response, default=str),
-#                     status=200,
-#                     mimetype='application/json')
-
-
-# if __name__ == '__main__':
-#     app.run(host="localhost", debug=True)
-from flask import Flask,Response,request,jsonify
+from flask import Flask, Response, request, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 import pymongo
 import json
 from bson.objectid import ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
+jwt = JWTManager(app)
+
+app.config["JWT_SECRET_KEY"] = "key"
 
 api_v1_cors_config = {
-  "origins":"*"
+    "origins": "*"
 }
 
-CORS(app,resources={
-  r"/*":api_v1_cors_config
+CORS(app, resources={
+    r"/*": api_v1_cors_config
 })
 
 
@@ -89,29 +27,42 @@ try:
     )
   
   db = mongo.CorrectionUI
-
+  user = db.Users
   mongo.server_info()
 
 except:
  print("Error - cannot connet to db")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.is_json:
+        email = request.json["email"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+
+    test = user.find_one({"email": email, "password": password})
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login Succeeded!", access_token=access_token), 200
+    else:
+        return jsonify(message="Bad Email or Password"), 400
+
 
 ###################################################
 
 #Fetching all batches
 
 @app.route('/batches',methods=["GET"])
-
+@jwt_required()
 def get_batches():
   try:
     data = list(db.batches.find())
-   # print(data)
-    for batch in data:
-        batch['_id'] = str(batch['_id'])
-    return Response(
-          response= json.dumps(data),
-          status=200,
-          mimetype="application/json"
-      )
+    return Response(response=json.dumps(data, default=str),
+                        status=200,
+                        mimetype="application/json")
 
   except Exception as ex:
     print(ex)
@@ -128,20 +79,16 @@ def get_batches():
 #  Fetch all documents
 
 @app.route("/documents",methods=['GET'])
-
+# @jwt_required()
 def get_documents():
    
     try:
       data = list(db.documents.find())
       
-      for document in data:
-        document['_id'] = str(document["_id"])
-
-      return Response(
-          response= json.dumps(data),
-          status=200,
-          mimetype="application/json"
-      )
+      return Response(response=json.dumps(data, default=str),
+                        status=200,
+                        mimetype="application/json")
+    
     except Exception as ex:
         print(ex)
     
@@ -150,21 +97,17 @@ def get_documents():
 # Fetch Documents list using batchId
 
 @app.route("/documents/<id>",methods=["GET"])
-
+# @jwt_required()
 def get_single_documents(id):
   
     try:
       data = list(db.documents.find({"batchId":id}))
       #print(data)
       
-      for document in data:
-        document['_id'] = str(document["_id"])
-
-      return Response(
-          response= json.dumps(data),
-          status=200,
-          mimetype="application/json"
-      )
+      return Response(response=json.dumps(data, default=str),
+                        status=200,
+                        mimetype="application/json")
+    
     except Exception as ex:
         print(ex)
     
@@ -173,20 +116,16 @@ def get_single_documents(id):
 #Fetching pages using document Id
 
 @app.route("/pages/<id>",methods=["GET"])
-
+# @jwt_required()
 def get_kvp_data(id):
    
     try:
       data = list(db.pages.find({"documentId":id}))
       
-      for ocrDataKvpUid in data:
-        ocrDataKvpUid ['_id'] = str(ocrDataKvpUid ["_id"])
+      return Response(response=json.dumps(data, default=str),
+                        status=200,
+                        mimetype="application/json")
 
-      return Response(
-          response= json.dumps(data),
-          status=200,
-          mimetype="application/json"
-      )
     except Exception as ex:
         print(ex)
     
@@ -195,6 +134,7 @@ def get_kvp_data(id):
 # updating page using raw data
 
 @app.route("/pages",methods=["PUT"])
+# @jwt_required()
 def put_ocr_data():
 
  try:    
@@ -226,4 +166,4 @@ def put_ocr_data():
 ###################################################
 
 if __name__ == "__main__":
-    app.run(port=80,debug=True)
+    app.run(port=5000,debug=True)
