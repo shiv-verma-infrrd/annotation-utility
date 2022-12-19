8b6715fbb8f09c52178fbba801ec9bcaac857188
abhishekinfrrd <112868050+abhishekinfrrd@users.noreply.github.com>
2022-12-19 13:42:32 +0530

    login functionality

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logout:TokenStorageService) { }
 
   ngOnInit(): void {
   }
 
+  onLogout(){
+    console.log("storage cleared");
+    this.logout.signOut();
+  }
 }
